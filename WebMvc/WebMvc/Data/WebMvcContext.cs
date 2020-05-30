150fc08ae034d0bacc618cc2f401bc5b909dec1b
Lara Galvani <laragalvanim@gmail.com>
2020-05-30 19:16:57 -0300

    CRUD Scaffolding

---
+++ WebMvc/WebMvc/Data/WebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebMvc.Models;
+
+namespace WebMvc.Data
+{
+    public class WebMvcContext : DbContext
+    {
+        public WebMvcContext (DbContextOptions<WebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebMvc.Models.Department> Department { get; set; }
+    }
+}
