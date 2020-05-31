607a5d6ab6b7d3ab314212c0442ef91b5afd5dff
Lara Galvani <laragalvanim@gmail.com>
2020-05-31 11:57:49 -0300

    SellersController

---
+++ WebMvc/WebMvc/Controllers/SellersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        //When the controller receives a call, then we need to return an action (in this case, View() called
+        //Index). To summarize, the controller forwarded a requisition to View().
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
